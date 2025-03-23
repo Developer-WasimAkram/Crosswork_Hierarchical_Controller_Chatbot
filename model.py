ae752c4f78442775b0263cb270a5619898015245
Developer-WasimAkram <noorewasim@gmail.com>
2025-03-23 12:50:02 +0530

    steamlit

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from langchain_groq import ChatGroq
 def get_openai_response( api_key):
     os.environ["OPENAI_API_KEY"] = api_key  # Use your OpenAI API key
     try: 
-        llm = ChatOpenAI(model="gpt-3.5-turbo", max_tokens=200)
+        llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.5)
         return llm
     except Exception as e:
         return f"Error: {e}"
@@ -14,7 +14,7 @@ def get_chatgroq_response(api_key):
     os.environ["GROQ_API_KEY"] = api_key
      
     try:
-        llm=ChatGroq(model="qwen-2.5-32b")        
+        llm=ChatGroq(model="qwen-2.5-32b",temperature=0.5)        
         return llm
     except Exception as e:
         return f"Error: {e}"
