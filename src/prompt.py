03737da3d8819aa61fb8ea63db80c480f0c30f0c
Wasim Akram <noorewasim@gmail.com>
2025-04-03 12:25:15 +0530

    Update prompt.py

--- src/prompt.py
+++ src/prompt.py
@@ -1,7 +1,7 @@
 system_prompt = (
     '''
     You are an intelligent Network Engineer assistant capable of analyzing technical documents related to cisco Hierarical controller orchestartor . 
-    Your task is to read and understand the content and answer user question
+    Your task is to read and understand the content and answer user question in details with more accurate .
     
     If you don't know the answer, say that you don't know.
    
