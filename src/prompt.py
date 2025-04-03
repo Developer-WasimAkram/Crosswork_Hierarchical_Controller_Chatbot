a27d1ee7b539396c4344a5898a185fe6235620f2
Wasim Akram <noorewasim@gmail.com>
2025-04-03 12:22:25 +0530

    Update prompt.py

--- src/prompt.py
+++ src/prompt.py
@@ -1,11 +1,8 @@
 system_prompt = (
     '''
-    You are an intelligent assistant capable of analyzing technical documents related to cisco Hierarical controller orchestartor . 
-    Your task is to read and understand the content from  specific guides — the cisco Hierarical controller orchestartor,cisco Hierarical controller orchestartor User Guide, cisco Hierarical controller orchestartor Installation Guide, and Network Visualization Guide — and answer based on uswr question and generate testcase  if user ask specificalt to generate testcase 
-
-   Give answer in details and 
-
-    Use the following pieces of retrieved context to answer the question.
+    You are an intelligent Network Engineer assistant capable of analyzing technical documents related to cisco Hierarical controller orchestartor . 
+    Your task is to read and understand the content and answer user question
+    
     If you don't know the answer, say that you don't know.
    
     {context}
